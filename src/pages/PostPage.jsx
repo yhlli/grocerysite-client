9019d4b831f5b339f0d1f8c5adcf1b1bcff00e54
Luke Li <36483872+yhlli@users.noreply.github.com>
2025-01-15 15:48:19 -0500

    newsapi integration

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -147,7 +147,12 @@ export default function PostPage() {
             <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
             <div className="author">
                 <p className="by">by </p>
-                <Link to={`/user/${postInfo.author.username}`} className="author">{postInfo.author.username}</Link>
+                {(postInfo.newsBot) ? (
+                    <a href={postInfo.pageUrl} target="_blank" className="author">{postInfo.uname}</a>
+                ) : (
+                    <Link to={`/user/${postInfo.author.username}`} className="author">{postInfo.author.username}</Link>
+                )}
+                
             </div>
             <div>
                 <p className="view">{(postInfo.views)} 
@@ -169,16 +174,16 @@ export default function PostPage() {
                     )}
                     {!isFavorite() && (
                         <Link className="edit-btn" onClick={addFavorite}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                             </svg>
                             Add to Favorites
                         </Link>
                     )}
                     {isFavorite() && favoritePosts.size > 0 && (
                         <Link className="edit-btn" onClick={removeFavorite}>
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
                             </svg>
                             Remove from Favorites
                         </Link>
@@ -194,7 +199,16 @@ export default function PostPage() {
                 </div>
             )}
             <div className="image">
-                <img src={`${address}/${postInfo.cover}`} alt="" />
+                {(postInfo.newsBot) ? (
+                    <>
+                        <img src={`${postInfo.cover}`} alt="" />
+                    </>
+                ) : (
+                    <>
+                        <img src={`${address}/${postInfo.cover}`} alt="" />
+                    </>
+                )}
+                
             </div>
             <div dangerouslySetInnerHTML={{__html:postInfo.content}} />
             {comments.length > 0 && (
